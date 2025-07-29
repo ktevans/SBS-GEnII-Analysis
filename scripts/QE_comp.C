44751a451b58edbfa5fa8132694d4a262bdb7477
Katherine Evans <89591378+ktevans@users.noreply.github.com>
2025-07-29 13:19:41 -0400

    Update QE_comp.C

--- scripts/QE_comp.C
+++ scripts/QE_comp.C
@@ -26,7 +26,7 @@
 void QE_comp(const char *kinematic)
 {
   TString inputfile = Form("/volatile/halla/sbs/ktevans/KateJackSBSAnalysis/KJ_parsed_GEn_pass2_%s_He3_100.root",kinematic);
-  TString outputfile = Form("/plots/parsed_GEn_pass2_%s_He3_dxdy.pdf",kinematic);
+  TString outputfile = Form("plots/parsed_GEn_pass2_%s_He3_dxdy.pdf",kinematic);
   TChain* T = new TChain("Parse");
   T->Add(inputfile);
 
