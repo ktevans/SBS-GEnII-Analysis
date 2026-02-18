54451a1cfb0ccf73d500a2cd77d7c6f7a20630b0
Katherine Evans <89591378+ktevans@users.noreply.github.com>
2026-02-18 12:40:51 -0500

    Update QE_comp.C

--- scripts/QE_comp.C
+++ scripts/QE_comp.C
@@ -148,7 +148,7 @@ void QE_comp(const char *kinematic)
       }
       
       W2_out = e_kine_W2;
-      helicity_out = helicity* (IHWP * IHWP_flip); //* IHWP;
+      helicity_out = helicity; //* (IHWP * IHWP_flip); //* IHWP;
 	//-1* IHWP * helicity * IHWP_flip;
     }
 
