2c6b39cd14026ac502d79938c686c80bd8f3df6e
Katherine Evans <89591378+ktevans@users.noreply.github.com>
2026-02-18 15:53:17 -0500

    Update SimDataComp.C

--- scripts/SimDataComp.C
+++ scripts/SimDataComp.C
@@ -279,8 +279,8 @@ void SimDataComp()
   FitFunc->SetParLimits(1,-1.0,1.0);  // proton shift
   FitFunc->SetParLimits(2,0.0,5.0);   // neutron scale
   FitFunc->SetParLimits(3,-1.0,1.0);  // neutron shift
-  FitFunc->SetParLimits(4,0.0,0.5);   // background scale
-  FitFunc->SetParLimits(5,0.0,3.0);   // background shift
+  FitFunc->SetParLimits(4,0.0,0.3);   // background scale
+  FitFunc->SetParLimits(5,-1.0,2.0);   // background shift
 
   h_data_dx->Fit(FitFunc,"0","",xmin,xmax);
 
