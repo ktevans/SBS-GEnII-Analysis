396ca08947032f5cfb816fd7c5d8b60ea4ba2717
Katherine Evans <89591378+ktevans@users.noreply.github.com>
2026-02-20 14:19:44 -0500

    Update SimDataComp.C

--- scripts/SimDataComp.C
+++ scripts/SimDataComp.C
@@ -293,13 +293,13 @@ void SimDataComp()
   FitFunc->SetNpx(numberBins);
   
   //----- GEN2 -----
-  double startpar[] = {1.0,-0.5,0.5,-0.7,0.05,0.0};
+  double startpar[] = {1.0,-0.5,0.5,-0.7,0.05,-1.0};
   FitFunc->SetParameters(startpar);
   FitFunc->SetParLimits(0,0.1,100);   // proton scale
   FitFunc->SetParLimits(1,-1.0,1.0);  // proton shift
   FitFunc->SetParLimits(2,0.1,100);   // neutron scale
   FitFunc->SetParLimits(3,-1.0,0.0);  // neutron shift
-  FitFunc->SetParLimits(4,0.05,0.2);   // background scale
+  FitFunc->SetParLimits(4,0.05,100);  // background scale
   FitFunc->SetParLimits(5,-1.0,1.0);  // background shift
 
   //----- GEN3 -----
