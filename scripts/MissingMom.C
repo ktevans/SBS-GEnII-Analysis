03ac2adefb25536ca985aed13b1f98dd8ce4bbc6
Katherine Evans <89591378+ktevans@users.noreply.github.com>
2026-02-24 13:13:43 -0500

    Update MissingMom.C

--- scripts/MissingMom.C
+++ scripts/MissingMom.C
@@ -62,24 +62,24 @@ void MissingMom(const char *kinematic)
   double weight;     T->SetBranchAddress("weight", &weight);
 
   //double dx_p_shift = 0.375;
-  double dx_p_shift = 0.5;
-  //double dx_p_shift = 0.375;
+  //double dx_p_shift = 0.5;
+  double dx_p_shift = 0.0;
 
   //double dx_n_shift = 0.7;
-  double dx_n_shift = 0.7;
   //double dx_n_shift = 0.7;
+  double dx_n_shift = 0.0;
 
   //double beam_e = 4.291;
-  double beam_e = 6.373;
-  //double beam_e = 8.448;
+  //double beam_e = 6.373;
+  double beam_e = 8.448;
 
   //double n_min = -0.95;
-  double n_min = -0.5;
-  //double n_min = -0.3;
+  //double n_min = -0.5;
+  double n_min = -0.3;
 
   //double n_max = 0.95;
-  double n_max = 0.5;
-  //double n_max = 0.3;
+  //double n_max = 0.5;
+  double n_max = 0.3;
 
   //Scan through all the entries in the TChain T
   //If the rootfiles are empty or don't exist, there will be 0 entries
