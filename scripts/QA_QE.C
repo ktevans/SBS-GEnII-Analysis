aa4d9efe9ac3071be07fc0b769efef373028686d
Katherine Evans <89591378+ktevans@users.noreply.github.com>
2026-03-18 10:07:07 -0400

    Update QA_QE.C

--- scripts/QA_QE.C
+++ scripts/QA_QE.C
@@ -30,7 +30,7 @@ void QA_QE(const char *kinematic)
 
   gErrorIgnoreLevel = kError; // Ignores all ROOT warnings
 
-  TString inputfile = Form("/volatile/halla/sbs/ktevans/KateJackSBSAnalysis/TEST_pass3/KJ_parsed_GEn_pass2_%s_He3_100.root",kinematic);
+  TString inputfile = Form("/volatile/halla/sbs/ktevans/KateJackSBSAnalysis/KJ_parsed_GEn_pass2_%s_He3_100.root",kinematic);
   TString outputfile = Form("plots/QA_parsed_GEn_pass2_%s_He3_dxdy.pdf",kinematic);
   TString outfile = Form("outfiles/QA_parsed_GEn_pass2_%s_He3_dxdy.root",kinematic);
   TFile *fout = new TFile(outfile,"RECREATE");
