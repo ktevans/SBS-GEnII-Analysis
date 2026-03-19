e69feb553c64eaa92b5babc187d4cb784d6b4661
Katherine Evans <89591378+ktevans@users.noreply.github.com>
2026-03-19 12:16:30 -0400

    Update SimDataComp.C

--- scripts/SimDataComp.C
+++ scripts/SimDataComp.C
@@ -86,7 +86,8 @@ void SimDataComp(int kin)
   if(kin == 2)
   {
 
-    data_file = "outfiles/parsed_GEn_pass2_GEN2_He3_dxdy.root";
+    //data_file = "outfiles/parsed_GEn_pass2_GEN2_He3_dxdy.root";
+    data_file = "outfiles/sean_GEn_pass2_GEN2_He3_dxdy.root";
     nucleon_sim_file = "outfiles/parsed_SIM_GEn_GEN2_He3_dxdy.root";
     inel_sim_file = "outfiles/parsed_SIM_IN_GEn_GEN2_He3_dxdy.root";
     title_words = "GEN2";
