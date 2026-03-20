d78bd31c36257491c56167bb82618b25e7e33646
Katherine Evans <89591378+ktevans@users.noreply.github.com>
2026-03-20 11:14:12 -0400

    Update SimDataComp.C

--- scripts/SimDataComp.C
+++ scripts/SimDataComp.C
@@ -288,9 +288,9 @@ void SimDataComp(int kin)
 
   //******************************************************
 
-  //double scale = h_data_dx->Integral();
+  double scale = h_data_dx->Integral();
   //h_data_dx->Scale(1.0/h_data_dx->Integral());
-  h_data_dx->Scale(1.0/scale);
+  //h_data_dx->Scale(1.0/scale);
   h_pos_hel_dx->Scale(1.0/h_pos_hel_dx->Integral());
   h_neg_hel_dx->Scale(1.0/h_neg_hel_dx->Integral());
   h_sim_proton_dx->Scale(1.0/h_sim_proton_dx->Integral());
