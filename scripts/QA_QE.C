4cf4016890eec607dcc9fa89911b877f9e3e93ee
Katherine Evans <89591378+ktevans@users.noreply.github.com>
2026-03-24 22:20:38 -0400

    Update QA_QE.C

--- scripts/QA_QE.C
+++ scripts/QA_QE.C
@@ -222,7 +222,7 @@ void QA_QE(const char *kinematic)
   h_ps_e_raw->GetXaxis()->SetTitle("bb.ps.e [GeV]");
   h_ps_e_raw->SetTitle("PreShower Energy with Global and Vertex Cuts");
 
-  TH2* h2_coin_W2 = new TH2L("h2_coin_W2", "Coin vs W2", 100.0, -2.0, 8.0, 80.0, -20.0, 20.0);
+  TH2* h2_coin_W2 = new TH2D("h2_coin_W2", "Coin vs W2", 100.0, -2.0, 8.0, 80.0, -20.0, 20.0);
   h2_coin_W2->GetXaxis()->SetTitle("e.kine.W2 [GeV]");
   h2_coin_W2->GetYaxis()->SetTitle("adc.coin [ns]");
   h2_coin_W2->SetTitle("Coincidence Time (HCal-BBCal) vs W2 with Global and Vertex Cuts");
@@ -505,7 +505,7 @@ void QA_QE(const char *kinematic)
           h_coin->Fill(adc_coin);
         }
 
-        h2_coin_W2->Fill(e_kine_W2,adc_coin);
+        //h2_coin_W2->Fill(e_kine_W2,adc_coin);
 
 
         if (bb_ps_e>0.2)
@@ -514,7 +514,7 @@ void QA_QE(const char *kinematic)
           if (abs(((bb_ps_e+bb_sh_e)/bb_tr_p)-1)<0.2)
           {
 
-            h2_pse_grclus->Fill(bb_ps_e,bb_gr_clus_size);
+            //h2_pse_grclus->Fill(bb_ps_e,bb_gr_clus_size);
 
             //T_data->Fill();
 
