a330ed24628d1a93209a3b1fbfb846d600a7ef30
Katherine Evans <89591378+ktevans@users.noreply.github.com>
2026-04-06 14:11:30 -0400

    Update MissingMom.C

--- scripts/MissingMom.C
+++ scripts/MissingMom.C
@@ -262,8 +262,8 @@ void MissingMom(const char *kinematic, int kin)
   TCanvas *c4 = new TCanvas("c4", "Profile Fitting", 100,100,700,700);
   c4->cd();
 
-  TF1 *fit1 = new TF1("fit1", "[0] + [1]*cos(x) + [2]*sin(x) + [3]*cos(2*x) + [4]*sin(2*x)", -3.5, 3.0); // + [5]*cos(3*x) + [6]*sin(3*x)
-  fit1->SetParameters(1,2,3,4,5);
+  TF1 *fit1 = new TF1("fit1", "[0] + [1]*cos(x) + [2]*sin(x) + [3]*cos(2*x) + [4]*sin(2*x) + [5]*cos(3*x) + [6]*sin(3*x) + [7]*cos(4*x) + [8]*sin(4*x)", -3.5, 3.0); // + [5]*cos(3*x) + [6]*sin(3*x)
+  fit1->SetParameters(1,2,3,4,5,6,7,8,9);
   fit1->SetLineColor(kRed);
 
   h_prof_pol_p->Fit("fit1");
