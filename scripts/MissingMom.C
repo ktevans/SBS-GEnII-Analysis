dca378f14b41253ccace90a0c266e2fad98802d4
Katherine Evans <89591378+ktevans@users.noreply.github.com>
2026-04-06 12:37:54 -0400

    Update MissingMom.C

--- scripts/MissingMom.C
+++ scripts/MissingMom.C
@@ -69,8 +69,8 @@ void MissingMom(const char *kinematic, int kin)
 
   if (kin == 2)
   {
-    dx_p_shift = 0.375;
-    dx_n_shift = 0.7;
+    dx_p_shift = 0.438;
+    dx_n_shift = 0.118;
     beam_e = 4.291;
     n_min = -0.95;
     n_max = 0.95;
