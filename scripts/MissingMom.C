a42117e08f8f7724cabe3ac22d690ca96e1a032a
Katherine Evans <89591378+ktevans@users.noreply.github.com>
2026-04-20 14:37:47 -0400

    Update MissingMom.C

--- scripts/MissingMom.C
+++ scripts/MissingMom.C
@@ -338,7 +338,7 @@ void MissingMom(const char *kinematic, int kin)
   //fitn->Draw("SAMES");
   //gStyle->SetOptFit(1111);
 
-  TF1 *fitn_low = new TF1("fitn_low", "[0] - [1]*TMath::Exp([2]x)", -3.5, dx_n_cut);
+  TF1 *fitn_low = new TF1("fitn_low", "[0]*x*x + [1]*x + [2]", -3.5, dx_n_cut);
   fitn_low->SetParameters(1.0,2.0,3.0);
   fitn_low->SetLineColor(kBlue);
   h_prof_pol_n_low->Fit("fitn_low");
