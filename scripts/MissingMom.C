f80240b5718956868d0a66c753756450a87ea93f
Katherine Evans <89591378+ktevans@users.noreply.github.com>
2026-04-23 11:59:37 -0400

    Update MissingMom.C

--- scripts/MissingMom.C
+++ scripts/MissingMom.C
@@ -382,8 +382,8 @@ void MissingMom(const char *kinematic, int kin)
   c4->cd(1);
   h_prof_pol_p_low->Draw();
 
-  TF1 *fitp_low = new TF1("fitp_low", "[0] + [1]*cos(x) + [2]*sin(x) + [3]*cos(2*x) + [4]*sin(2*x)", -4.0, dx_p_cut);
-  fitp_low->SetParameters(1.0,2.0,3.0,4.0,5.0);
+  TF1 *fitp_low = new TF1("fitp_low", "[0] + [1]*cos(x) + [2]*sin(x) + [3]*cos(2*x) + [4]*sin(2*x)+ [5]*cos(3*x) + [6]*sin(3*x)", -4.0, dx_p_cut);
+  fitp_low->SetParameters(1.0,2.0,3.0,4.0,5.0,6.0,7.0);
   fitp_low->SetLineColor(kBlue);
   h_prof_pol_p_low->Fit("fitp_low");
   fitp_low->Draw("SAMES");
