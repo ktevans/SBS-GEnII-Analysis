9a8313a2f1e5fd8afc21e08c7c5cb81c71f53b93
Katherine Evans <89591378+ktevans@users.noreply.github.com>
2026-04-29 17:12:35 -0400

    Update dy_cut.C

--- scripts/dy_cut.C
+++ scripts/dy_cut.C
@@ -118,7 +118,7 @@ void dy_cut()
   TH1D* h_dx_anti = new TH1D("h_dx_anti", ";h_dx_anti", 90.0, -6.0, 3.0);
   h_dx_anti->GetXaxis()->SetTitle("dx [m]");
 
-  TH2D* h_dxdy = new TH2D("h_dxdy", ";h_dxdy", 90.0, -6.0, 3.0, 60.0, -3.0, 3.0);
+  TH2D* h_dxdy = new TH2D("h_dxdy", ";h_dxdy", 60.0, -3.0, 3.0, 90.0, -6.0, 3.0);
   h_dxdy->GetXaxis()->SetTitle("dy [m]");
   h_dxdy->GetYaxis()->SetTitle("dx [m]");
 
@@ -129,11 +129,13 @@ void dy_cut()
 
     if ((IHWP==-1 || IHWP==1) && (bb_tr_r_x-0.9*bb_tr_r_th)>optics_valid_min && (bb_tr_r_x-0.9*bb_tr_r_th)<optics_valid_max && bb_gr_clus_track==0 && bb_ps_e>0.2 && gem_tr_ngoodhits>=3 && gem_tr_chi2ndf<=15 && abs(adc_coin-coin_mean)<(coin_sigma) && bb_gr_clus_size>2 && abs(((bb_ps_e+bb_sh_e)/bb_tr_p)-0.97)<0.2 && abs(e_kine_W2-1.0)<0.5)
     {
-      h_dxdy->Fill(dy_hcal,dx_hcal);
-      h_dy->Fill(dy_hcal);
+      //h_dxdy->Fill(dy_hcal,dx_hcal);
+      //h_dy->Fill(dy_hcal);
 
-      if (dy_hcal>(dy_mean+dy_sigma)||dy_hcal<(dy_mean-dy_sigma))
+      if (dy_hcal>(dy_mean+dy_sigma) || dy_hcal<(dy_mean-dy_sigma))
       {
+        h_dxdy->Fill(dy_hcal,dx_hcal);
+        h_dy->Fill(dy_hcal);
         h_dx_anti->Fill(dx_hcal);
       }
     }
