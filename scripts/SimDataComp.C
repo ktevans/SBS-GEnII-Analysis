f81cce61151cf5393ddd91caa44300b84bad9466
Katherine Evans <89591378+ktevans@users.noreply.github.com>
2026-04-29 17:54:42 -0400

    Update SimDataComp.C

--- scripts/SimDataComp.C
+++ scripts/SimDataComp.C
@@ -99,8 +99,8 @@ void SimDataComp(int kin)
     inel_sim_file = "outfiles/parsed_SIM_IN_GEn_GEN2_He3_dxdy.root";
     pol_func_file = "outfiles/parsed_GEn_pass2_GEN2_simulation.root";
     title_words = "GEN2";
-    dx_min_d = -4.0;
-    dx_min_i = -4;
+    dx_min_d = -5.0;
+    dx_min_i = -5;
     dx_max_d = 2.0;
     dx_max_i = 2;
     cout<<"\nHi!\n";
