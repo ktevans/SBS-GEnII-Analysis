52ece112af0cc79c62d89da0d2e44d0ca477ac99
Katherine Evans <89591378+ktevans@users.noreply.github.com>
2026-05-20 13:42:23 -0400

    Update coin_accidentals.C

--- scripts/coin_accidentals.C
+++ scripts/coin_accidentals.C
@@ -312,7 +312,7 @@ void coin_accidentals(const char *kinematic)
       }
 
       //if(abs(adc_coin-coin_mean)>8*coin_sigma)
-      if(adc_coin>-25 && adc_coin<-20)
+      if(adc_coin>-20 && adc_coin<-15)
       {
         h_dx_anti_coin->Fill(dx_hcal);
 
