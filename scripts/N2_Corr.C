9fa7b45c354fc121213739a36358a1f8e2d1387a
Katherine Evans <89591378+ktevans@users.noreply.github.com>
2026-07-14 18:27:23 -0400

    Update N2_Corr.C

--- scripts/N2_Corr.C
+++ scripts/N2_Corr.C
@@ -36,27 +36,42 @@ void N2_Corr(const char *kinematic)
 
   TTree *T_sim = new TTree("T_sim", "Analysis Data Tree");
 
-  double dx_out_He3, dy_out_He3, fnucl_out_He3, weight_out_He3, dx_out_N2, dy_out_N2, fnucl_out_N2, weight_out_N2;
-  T_sim->Branch("dx.He3", &dx_out_He3, "dx.He3/D");
+  double dx_out_p_He3, dx_out_n_He3, dy_out_He3, fnucl_out_He3, weight_out_He3, dx_out_p_N2, dx_out_n_N2, dy_out_N2, fnucl_out_N2, weight_out_N2;
+  T_sim->Branch("dx.p.He3", &dx_out_p_He3, "dx.p.He3/D");
+  T_sim->Branch("dx.n.He3", &dx_out_n_He3, "dx.n.He3/D");
   T_sim->Branch("dy.He3", &dy_out_He3, "dy.He3/D");
   T_sim->Branch("fnucl.He3", &fnucl_out_He3, "fnucl.He3/D");
   T_sim->Branch("weight.He3", &weight_out_He3, "weight.He3/D");
-  T_sim->Branch("dx.N2", &dx_out_N2, "dx.N2/D");
+  T_sim->Branch("dx.p.N2", &dx_out_p_N2, "dx.p.N2/D");
+  T_sim->Branch("dx.n.N2", &dx_out_n_N2, "dx.n.N2/D");
   T_sim->Branch("dy.N2", &dy_out_N2, "dy.N2/D");
   T_sim->Branch("fnucl.N2", &fnucl_out_N2, "fnucl.N2/D");
   T_sim->Branch("weight.N2", &weight_out_N2, "weight.N2/D");
 
-  TH1D* h_N2_tot_dx = new TH1D("h_N2_tot_dx","Total N2 Events", 130, -6.0, 7.0);
-  h_N2_tot_dx->GetXaxis()->SetTitle("dx [m]");
+  TH1D* h_N2_tot_dx_p = new TH1D("h_N2_tot_dx_p","Total N2 Proton Events", 130, -6.0, 7.0);
+  h_N2_tot_dx_p->GetXaxis()->SetTitle("dx [m]");
 
-  TH1D* h_N2_QE_dx = new TH1D("h_N2_QE_dx","QE N2 Events", 130, -6.0, 7.0);
-  h_N2_QE_dx->GetXaxis()->SetTitle("dx [m]");
+  TH1D* h_N2_QE_dx_p = new TH1D("h_N2_QE_dx_p","QE N2 Proton Events", 130, -6.0, 7.0);
+  h_N2_QE_dx_p->GetXaxis()->SetTitle("dx [m]");
 
-  TH1D* h_He3_tot_dx = new TH1D("h_He3_tot_dx","Total He3 Events", 130, -6.0, 7.0);
-  h_He3_tot_dx->GetXaxis()->SetTitle("dx [m]");
+  TH1D* h_He3_tot_dx_p = new TH1D("h_He3_tot_dx_p","Total He3 Proton Events", 130, -6.0, 7.0);
+  h_He3_tot_dx_p->GetXaxis()->SetTitle("dx [m]");
 
-  TH1D* h_He3_QE_dx = new TH1D("h_He3_QE_dx","QE He3 Events", 130, -6.0, 7.0);
-  h_He3_QE_dx->GetXaxis()->SetTitle("dx [m]");
+  TH1D* h_He3_QE_dx_p = new TH1D("h_He3_QE_dx_p","QE He3 Proton Events", 130, -6.0, 7.0);
+  h_He3_QE_dx_p->GetXaxis()->SetTitle("dx [m]");
+
+  TH1D* h_N2_tot_dx_n = new TH1D("h_N2_tot_dx_n","Total N2 Neutron Events", 130, -6.0, 7.0);
+  h_N2_tot_dx_n->GetXaxis()->SetTitle("dx [m]");
+
+  TH1D* h_N2_QE_dx_n = new TH1D("h_N2_QE_dx_n","QE N2 Neutron Events", 130, -6.0, 7.0);
+  h_N2_QE_dx_n->GetXaxis()->SetTitle("dx [m]");
+
+  TH1D* h_He3_tot_dx_n = new TH1D("h_He3_tot_dx_n","Total He3 Neutron Events", 130, -6.0, 7.0);
+  h_He3_tot_dx_n->GetXaxis()->SetTitle("dx [m]");
+
+  TH1D* h_He3_QE_dx_n = new TH1D("h_He3_QE_dx_n","QE He3 Neutron Events", 130, -6.0, 7.0);
+  h_He3_QE_dx_n->GetXaxis()->SetTitle("dx [m]");
+
 
   TChain* T_He3 = new TChain("Tout");
   T_He3->Add(inputfile_He3);
@@ -87,13 +102,31 @@ void N2_Corr(const char *kinematic)
   {
     T_He3->GetEntry(iev);
 
-    h_He3_tot_dx->Fill(dx_hcal_He3,weight_hcal_He3);
+    if(fnucl_hcal_He3==1.0)
+    {
+      h_He3_tot_dx_p->Fill(dx_hcal_He3,weight_hcal_He3);
+    }
+
+    if(fnucl_hcal_He3==0.0)
+    {
+      h_He3_tot_dx_n->Fill(dx_hcal_He3,weight_hcal_He3);
+    }
 
     if(abs(e_kine_W2_He3-1.0)<0.5 && bb_ps_e_He3>0.2 && abs(((bb_ps_e_He3+bb_sh_e_He3)/bb_tr_p_He3)-0.97)<0.2 && abs(bb_tr_vz_He3)<0.27 && sbs_hcal_e_He3>0.025)
     {
-      h_He3_QE_dx->Fill(dx_hcal_He3,weight_hcal_He3);
 
-      dx_out_He3 = dx_hcal_He3;
+      if(fnucl_hcal_He3==1.0)
+      {
+        h_He3_QE_dx_p->Fill(dx_hcal_He3,weight_hcal_He3);
+        dx_out_p_He3 = dx_hcal_He3;
+      }
+
+      if(fnucl_hcal_He3==0.0)
+      {
+        h_He3_QE_dx_n->Fill(dx_hcal_He3,weight_hcal_He3);
+        dx_out_n_He3 = dx_hcal_He3;
+      }
+
       dy_out_He3 = dy_hcal_He3;
       fnucl_out_He3 = fnucl_hcal_He3;
       weight_out_He3 = weight_hcal_He3;
@@ -131,13 +164,31 @@ void N2_Corr(const char *kinematic)
   {
     T_N2->GetEntry(iev);
 
-    h_N2_tot_dx->Fill(dx_hcal_N2,weight_hcal_N2);
+    if(fnucl_hcal_N2==1.0)
+    {
+      h_N2_tot_dx_p->Fill(dx_hcal_N2,weight_hcal_N2);
+    }
+
+    if(fnucl_hcal_N2==0.0)
+    {
+      h_N2_tot_dx_n->Fill(dx_hcal_N2,weight_hcal_N2);
+    }
 
     if(abs(e_kine_W2_N2-1.0)<0.5 && bb_ps_e_N2>0.2 && abs(((bb_ps_e_N2+bb_sh_e_N2)/bb_tr_p_N2)-0.97)<0.2 && abs(bb_tr_vz_N2)<0.27 && sbs_hcal_e_N2>0.025)
     {
-      h_N2_QE_dx->Fill(dx_hcal_N2,weight_hcal_N2);
 
-      dx_out_N2 = dx_hcal_N2;
+      if(fnucl_hcal_N2==1.0)
+      {
+        h_N2_QE_dx_p->Fill(dx_hcal_N2,weight_hcal_N2);
+        dx_out_p_N2 = dx_hcal_N2;
+      }
+
+      if(fnucl_hcal_N2==0.0)
+      {
+        h_N2_QE_dx_n->Fill(dx_hcal_N2,weight_hcal_N2);
+        dx_out_n_N2 = dx_hcal_N2;
+      }
+
       dy_out_N2 = dy_hcal_N2;
       fnucl_out_N2 = fnucl_hcal_N2;
       weight_out_N2 = weight_hcal_N2;
@@ -146,28 +197,43 @@ void N2_Corr(const char *kinematic)
     }
   }//end event loop
 
-  TH1D* hN2frac = (TH1D*) h_N2_QE_dx->Clone("hN2frac");
-  hN2frac->Divide(h_N2_tot_dx);
+  TH1D* hN2frac_n = (TH1D*) h_N2_QE_dx_n->Clone("hN2frac_n");
+  hN2frac_n->Divide(h_N2_tot_dx_n);
 
-  TH1D* hHe3frac = (TH1D*) h_He3_QE_dx->Clone("hHe3frac");
-  hHe3frac->Divide(h_He3_tot_dx);
-  hHe3frac->Scale(14.0);
+  TH1D* hHe3frac_n = (TH1D*) h_He3_QE_dx_n->Clone("hHe3frac_n");
+  hHe3frac_n->Divide(h_He3_tot_dx_n);
+  hHe3frac_n->Scale(14.0);
 
-  TH1D* hN2dilution = (TH1D*) hN2frac->Clone("hN2dilution");
-  hN2dilution->Divide(hHe3frac);
-  hN2dilution->SetTitle("N2 Dilution Simulation Scaling");
+  TH1D* hN2dilution_n = (TH1D*) hN2frac_n->Clone("hN2dilution_n");
+  hN2dilution_n->Divide(hHe3frac_n);
+  hN2dilution_n->SetTitle("N2 Neutron Dilution Simulation Scaling");
+
+  TH1D* hN2frac_p = (TH1D*) h_N2_QE_dx_p->Clone("hN2frac_p");
+  hN2frac_p->Divide(h_N2_tot_dx_p);
+
+  TH1D* hHe3frac_p = (TH1D*) h_He3_QE_dx_p->Clone("hHe3frac_p");
+  hHe3frac_p->Divide(h_He3_tot_dx_p);
+  hHe3frac_p->Scale(7.0);
+
+  TH1D* hN2dilution_p = (TH1D*) hN2frac_p->Clone("hN2dilution_p");
+  hN2dilution_p->Divide(hHe3frac_p);
+  hN2dilution_p->SetTitle("N2 Proton Dilution Simulation Scaling");
 
   TCanvas *c1 = new TCanvas("c1","N2 Dilution as a Function of dx",100,100,700,700);
   c1->cd();
-  hN2dilution->Draw();
+  hN2dilution_p->Draw();
+  hN2dilution_n->Draw("SAMES");
 
   printf("You've completed the script!\n");
 
   //Save the canvas to a pdf
   c1->Print(outputfile);
 
-  hN2frac->Write();
-  hHe3frac->Write();
-  hN2dilution->Write();
+  hN2frac_p->Write();
+  hHe3frac_p->Write();
+  hN2dilution_p->Write();
+  hN2frac_n->Write();
+  hHe3frac_n->Write();
+  hN2dilution_n->Write();
   fout->Write();
 }
