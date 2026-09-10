1a06d19a06fb24be23be6efe304ae46748bea974
Katherine Evans <89591378+ktevans@users.noreply.github.com>
2026-09-09 20:43:34 -0400

    Update BkgComp.C

--- scripts/BkgComp.C
+++ scripts/BkgComp.C
@@ -47,14 +47,45 @@ void BkgComp()
   TFile *T_InelFile = TFile::Open(InelFile);
   TH1D *h_dx = nullptr;
   T_InelFile->GetObject("h_dx", h_dx);
-
   //h_dx->Scale(1.0/h_dx->Integral());
   h_dx->SetLineColor(kGreen);
 
+  TChain* Tout = new TChain("Tout");
+  Tout->Add(DataFile);
+
+  Double_t dx;            Tout->SetBranchAddress("dx", &dx);
+  Double_t dy;            Tout->SetBranchAddress("dy", &dy);
+  int helicity;           Tout->SetBranchAddress("helicity", &helicity);
+  double coin;            Tout->SetBranchAddress("adc.coin", &coin);
+  double ps_e;            Tout->SetBranchAddress("bb.ps.e", &ps_e);
+  double sh_e;            Tout->SetBranchAddress("bb.sh.e", &sh_e);
+  double W2;              Tout->SetBranchAddress("e.kine.W2", &W2);
+  double Q2;              Tout->SetBranchAddress("e.kine.Q2", &Q2);
+  double tr_p;            Tout->SetBranchAddress("bb.tr.p", &tr_p);
+  double grinch_track;    Tout->SetBranchAddress("bb.grinch_tdc.clus.trackindex", &grinch_track);
+  double grinch_clusSize; Tout->SetBranchAddress("bb.grinch_tdc.clus.size", &grinch_clusSize);
+
+  TH1D* h_dx_acc = new TH1D("h_dx_acc","Accidentals", 100, -3.0, 2.0);
+  h_dx_acc->GetXaxis()->SetTitle("dx [m]");
+  h_dx_acc->SetLineColor(kRed);
+
+  for (size_t iev = 0; iev < Tout->GetEntries(); iev++)
+  {
+    Tout->GetEntry(iev);
+
+    if(W2>0.4 && W2<1.6 && abs(coin+0.47385)>6.0 && grinch_track==0.0 && grinch_clusSize>=3.0 && ps_e>0.2 && abs(((ps_e+sh_e)/tr_p)-0.97)<0.2 && abs(dy)<0.88)
+    {
+      h_dx_acc->Fill(dx);
+    }
+
+  }
+
+
   TCanvas *c1 = new TCanvas("c1", "Background Shapes", 100,100,800,800);
   c1->cd();
-  scaled_h_sim_nucleons->Draw("HIST");
-  h_N2_scaled->Draw("HIST SAMES");
+  //scaled_h_sim_nucleons->Draw("HIST");
+  h_N2_scaled->Draw("SAMES");
   h_dx->Draw("HIST SAMES");
+  h_dx_acc->Draw("HIST SAMES");
 
 }
